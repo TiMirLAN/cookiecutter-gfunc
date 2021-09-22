c3b604ac9bc59c889168c6283f58a1b3cbe3af04
Tim Mironov <timirlan666@gmail.com>
2021-09-22 22:26:24 +0500

    linted

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     sentry_dsn: Optional[str]
 
-    log_level: str = 'INFO'
+    log_level: str = "INFO"
     log_format: Optional[str]
 
 
